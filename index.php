2e5d07609d31854ecd23a7518759918fc7a16fa1
Ayesh Karunaratne <ayesh@aye.sh>
2021-10-07 11:41:15 +0530

    Move the index.php testing logic to separate file

--- index.php
+++ index.php
@@ -2,15 +2,10 @@
 declare(strict_types = 1);
 
 require __DIR__ . '/src/Redirector.php';
-$uri = $_SERVER['REQUEST_URI'] ?? $argv[1] ?? null;
-assert($uri !== null);
-$redirector = new Redirector($uri);
+$redirector = new Redirector($_SERVER['REQUEST_URI']);
 $url = $redirector->getTargetUrl();
 
 //echo $url;
 http_response_code(301);
 header('Location: ' . $url);
-// setting content-type text/plain ensures there's no possibility of xss/javascript injection.. probably not needed with the Location header above though.
-header("Content-Type: text/plain; charset=UTF-8");
-echo "you are being redirected to: {$url}";
 die();
